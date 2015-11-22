040d81a9feb04552b7d0187a4d63d5f061da68a9
zsiva <m.carrizo.ibarra@gmail.com>
2015-11-22 20:08:43 +0100

    Add edit func to the books list

--- frontend/src/components/booksList/index.js
+++ frontend/src/components/booksList/index.js
@@ -1,11 +1,20 @@
 module.exports = angular.module('books.list.bookListController', [
     require('services/book-service').name,
-    require('components/createBook').name
+    require('components/createBook').name,
+    require('components/updateBook').name
     ])
     .controller('bookListController', bookListController);
 
-function bookListController (bookService) {
+function bookListController (bookService, $scope) {
     const vm = this;
 
+    vm.hidden = true;
     vm.booksList = bookService.booksList();
+
+    vm.editBook = function (bookData) {
+        vm.hidden = false;
+        $scope.$broadcast('editBook', {
+            data: bookData
+        })
+    };
 }
