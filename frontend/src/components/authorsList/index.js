6b373265640d7ad11d51322f638bf1c817d77139
zsiva <m.carrizo.ibarra@gmail.com>
2015-12-07 01:14:33 +0100

    update authors list

--- frontend/src/components/authorsList/index.js
+++ frontend/src/components/authorsList/index.js
@@ -1,10 +1,18 @@
 module.exports = angular.module('books.list.authorsListController', [])
     .controller('authorListController', authorListController);
 
-function authorListController (authorService) {
+function authorListController (authorService, $http) {
     const vm = this;
 
     vm.hidden = true;
     vm.authorsList = authorService.authorsList();
+
+    vm.showBooks = function (authorId) {
+      console.log(authorId);
+      return $http.get('/api/books/' + authorId).then( function (res) {
+          //vm.authorBooks = res.data;
+          console.log('books for this author', res.data);
+      });
+    }
 
 }
