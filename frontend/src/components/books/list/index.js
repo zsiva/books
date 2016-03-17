7436842ab83f89e0e7efe4a058366128a43619e0
zsiva <m.carrizo.ibarra@gmail.com>
2016-03-17 18:12:39 +0100

    adapt controller to new service

--- frontend/src/components/books/list/index.js
+++ frontend/src/components/books/list/index.js
@@ -1,30 +1,16 @@
-module.exports = angular.module('books.list.bookListController', [
-    require('services/book-service').name,
-    require('components/books/create').name,
-    require('components/books/edit').name
-    ])
-    .controller('bookListController', bookListController);
+import { ItemService } from '../../../services/item-service';
 
-function bookListController (bookService, $scope, modalService) {
-    const vm = this;
+class BookListController {
+    constructor(itemFactory) {
+        const vm = this;
 
-    vm.hidden = true;
-    vm.booksList = bookService.getBooksList();
-
-    vm.sortType     = 'title';
-    vm.sortReverse  = false;
-
-    vm.deleteBook = function(id) {
-      var modalOptions = {
-          closeButtonText: 'Cancel',
-          actionButtonText: ' Delete',
-          headerText: 'Delete book',
-          bodyText: 'Are you sure you want to delete it?',
-          displayAction: true,
-          actionClass: 'btn-danger fa fa-trash'
-      };
-      modalService.showModal(modalOptions).then(function (modalScope) {
-        bookService.deleteBook(id);
-      });
-    };
+        vm.itemService = itemFactory;
+        vm.hidden = true;
+        vm.sortType = 'title';
+        vm.sortReverse = false;
+        vm.itemService.getItems('authorbooks').then(result => vm.booksList = result);
+    }
 }
+
+module.exports = angular.module('books.list.bookListController', [])
+    .controller('bookListController', BookListController);
