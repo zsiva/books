25d378e9f332545780441abe2cfac40d6129a579
zsiva <m.carrizo.ibarra@gmail.com>
2016-03-19 00:50:38 +0100

    split routes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,19 @@
-require('angular');
+//require('angular');
 require('angular-ui-router');
 require('angular-ui-bootstrap');
 require('angular-slugify');
 
-import ROUTES from './constants/routes';
-import STATES from './constants/states';
+import angular from 'angular';
+
+import services from './services/services';
+import routes from './routes';
 
 const app = angular.module('books', [
     'ui.router',
     'ui.bootstrap',
     'slugifier',
-    'books.itemService',
-    require('./services/book-service').name,
+    services.name,
+    routes.name,
     require('./components/books/list').name,
     require('./components/books/info').name,
     require('./components/books/create').name,
@@ -21,81 +23,6 @@ const app = angular.module('books', [
     require('./components/authors/info').name,
     require('./services/author-service').name,
     require('./services/modal-service').name,
-    require('./services/book-service').name,
     require('./components/shared/rating').name
 
 ]);
-app.config(setUpRoutes);
-
-function setUpRoutes ($stateProvider, $locationProvider) {
-    $locationProvider.html5Mode(true);
-    $stateProvider
-        .state(STATES.BOOKS_LIST, {
-            url: ROUTES.BOOKS,
-            template: require('./components/books/list/template.html'),
-            controller: 'bookListController',
-            controllerAs: 'vm'
-        })
-        .state(STATES.BOOK_INFO, {
-            url: ROUTES.BOOK_INFO,
-            template: require('./components/books/info/template.html'),
-            controller: 'bookInfoController',
-            controllerAs: 'vm',
-            params: {
-              bookSlug: '',
-              bookId: ''
-            },
-            resolve: {
-                bookData: function ($http, $stateParams) {
-                    return $http.get('/api/books/' + $stateParams.bookSlug).then( function (res) {
-                        return res.data[0];
-                    });
-                },
-                authors: function ($http, authorService) {
-                    return $http.get('/api/authors').then( function (res) {
-                        authorService.initAuthors(res.data);
-                    });
-                }
-            }
-        })
-        .state(STATES.BOOK_CREATE, {
-            url: ROUTES.BOOK_CREATE,
-            template: require('./components/books/create/template.html'),
-            controller: 'bookCreateController',
-            controllerAs: 'vm'
-        })
-        .state(STATES.AUTHORS_LIST, {
-            url: ROUTES.AUTHORS,
-            template: require('./components/authors/list/template.html'),
-            controller: 'authorListController',
-            controllerAs: 'vm',
-            resolve: {
-                authors: function ($http, authorService) {
-                    return $http.get('/api/authors').then( function (res) {
-                        authorService.initAuthors(res.data);
-                    });
-                }
-            }
-        })
-        .state(STATES.AUTHOR_INFO, {
-            url: ROUTES.AUTHOR_INFO,
-            params: {
-              authorSlug: '',
-              authorId: ''
-            },
-            template: require('./components/authors/info/template.html'),
-            controller: 'authorInfoController',
-            controllerAs: 'vm',
-            resolve: {
-                authorData: function ($http, $stateParams) {
-                    return $http.get('/api/authors/' + $stateParams.authorId).then( function (res) {
-                      return res.data;
-                    });
-                }
-            }
-        })
-        .state('index', {
-            url: '/',
-            template: 'Hello'
-        });
-}
---
+++ frontend/src/routes.js
@@ -0,0 +1,90 @@
+import ROUTES from './constants/routes';
+import STATES from './constants/states';
+
+/*@ngInject*/
+function routes ($stateProvider, $locationProvider) {
+    $locationProvider.html5Mode(true);
+    $stateProvider
+        .state(STATES.BOOKS_LIST, {
+            url: ROUTES.BOOKS,
+            template: require('./components/books/list/template.html'),
+            controller: 'bookListController',
+            controllerAs: 'vm'
+        })
+        .state(STATES.BOOK_INFO, {
+            url: ROUTES.BOOK_INFO,
+            template: require('./components/books/info/template.html'),
+            controller: 'bookInfoController',
+            controllerAs: 'vm',
+            params: {
+              bookSlug: '',
+              bookId: ''
+            },
+            resolve: {
+                bookData: function ($http, $stateParams) {
+                    return $http.get('/api/books/' + $stateParams.bookSlug).then( function (res) {
+                        return res.data[0];
+                    });
+                },
+                authors: function ($http, authorService) {
+                    return $http.get('/api/authors').then( function (res) {
+                        authorService.initAuthors(res.data);
+                    });
+                }
+            }
+        })
+        .state(STATES.BOOK_CREATE, {
+            url: ROUTES.BOOK_CREATE,
+            template: require('./components/books/create/template.html'),
+            controller: 'bookCreateController',
+            controllerAs: 'vm'
+        })
+        .state(STATES.AUTHORS_LIST, {
+            url: ROUTES.AUTHORS,
+            template: require('./components/authors/list/template.html'),
+            controller: 'authorListController',
+            controllerAs: 'vm',
+            resolve: {
+                authors: function ($http, authorService) {
+                    return $http.get('/api/authors').then( function (res) {
+                        authorService.initAuthors(res.data);
+                    });
+                }
+            }
+        })
+        .state(STATES.AUTHOR_INFO, {
+            url: ROUTES.AUTHOR_INFO,
+            params: {
+              authorSlug: '',
+              authorId: ''
+            },
+            template: require('./components/authors/info/template.html'),
+            controller: 'authorInfoController',
+            controllerAs: 'vm',
+            resolve: {
+                authorData: function ($http, $stateParams) {
+                    return $http.get('/api/authors/' + $stateParams.authorId).then( function (res) {
+                      return res.data;
+                    });
+                }
+            }
+        })
+        .state('ideas', {
+            url: '/ideas',
+            template: '<span>Test</span>',
+            controller: 'IdeaController',
+            controllerAs: 'ideaCtrl',
+            resolve: {
+                ideas(Idea) {
+                    console.log('testing service');
+                    return Idea.all();
+                }
+            }
+        })
+        .state('index', {
+            url: '/',
+            template: 'Hello'
+        });
+}
+export default angular.module('app.routes', [])
+  .config(routes);
