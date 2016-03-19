60c44dfc68267f6b22454d5f6b2143753eb2ec53
zsiva <m.carrizo.ibarra@gmail.com>
2016-03-19 01:50:39 +0100

    add author service to service file

--- frontend/src/services/services.js
+++ frontend/src/services/services.js
@@ -1,9 +1,11 @@
 import ItemService from './item.service';
 import BookService from './book.service';
+import AuthorService from './author.service';
 
 var moduleName = 'books.services';
 module.exports = angular.module(moduleName, [])
   .service('ItemService', ItemService)
-  .service('BookService', BookService);
+  .service('BookService', BookService)
+  .service('AuthorService', AuthorService);
 
 export default moduleName;
