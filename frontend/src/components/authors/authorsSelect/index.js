1d68b80cdaabad02d5b4cb05346d26d3b997ca7a
zsiva <m.carrizo.ibarra@gmail.com>
2016-03-20 23:24:52 +0100

    update select authors directive

--- frontend/src/components/authors/authorsSelect/index.js
+++ frontend/src/components/authors/authorsSelect/index.js
@@ -1,27 +1,30 @@
-module.exports = angular.module('books.selectAuthorsList', [
-  require('services/author-service').name
-])
-.directive('selectAuthorsList', selectAuthorsList);
+/**
+* @desc directive that creates a dropdown with all the authors
+* @example <select-authors-list></select-authors-list>
+*/
 
-function selectAuthorsList() {
-    return {
-        restrict: 'E',
-        bindToController: true,
-        controllerAs: 'vm',
-        scope: {
-          authorName: '=',
-          selectModel: '='
-        },
-        template: require('./template.html'),
-        controller: selectAuthorsListController
-    };
-}
-
-function selectAuthorsListController(authorService) {
-    const vm = this;
-    vm.hidden = true;
-    vm.authorsList = authorService.getAuthorsList();
-
-    vm.openNewAuthor = function (author) {
+class SelectAuthorsList {
+    constructor() {
+        this.template = require('./template.html');
+        this.restrict = 'EA';
+        this.controllerAs = 'vm';
+        this.bindToController = true;
+        this.authorsList = [];
+        this.controller = SelectAuthorsListController;
+        this.scope = {
+            selectModel: '='
+        }
     }
 }
+
+class SelectAuthorsListController {
+    constructor (AuthorService) {
+        Promise.resolve(AuthorService.getAllItems()).then((data) => {
+            console.log(data);
+            this.authorsList = data;
+        });
+    }
+}
+
+module.exports = angular.module('books.selectAuthorsList', [])
+    .directive('selectAuthorsList', () => new SelectAuthorsList);
