d356fbf5b81d89b588f339cb852b74c112861a58
zsiva <m.carrizo.ibarra@gmail.com>
2016-03-22 23:25:33 +0100

    fix info controller

--- frontend/src/components/authors/info/index.js
+++ frontend/src/components/authors/info/index.js
@@ -5,15 +5,13 @@ class AuthorInfoController {
         this.authorService = AuthorService;
         this.editorEnabled = false;
         this.authorData = authorData;
-
-        this.author = authorData.author_id;
-        this.authorName = authorData.name;
-        this.booksList = authorData.books;
+        this.booksList = authorData.books_id;
+        this.$state = $state;
     }
 
     saveAuthor (){
-        this.bookService.updateItem(this.authorData);
-        $state.go(STATES.AUTHORS_LIST);
+        this.authorService.updateItem(this.authorData);
+        this.$state.go(STATES.AUTHORS_LIST);
     };
 }
 
